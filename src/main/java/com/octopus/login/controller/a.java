80d3bd4fca102f3ae7fde6a7f0f95b5b410d2ac8
hermeswing <hermeswing@naver.com>
2024-01-26 00:44:46 +0900

    파일테스트4

--- src/main/java/com/octopus/login/controller/a.java
+++ src/main/java/com/octopus/login/controller/a.java
@@ -3,5 +3,9 @@ package com.octopus.login.controller;
 public class a {
     public void test() {
         System.out.println("이렇게 한다.");
+        System.out.println("이렇게 한다.");
+        System.out.println("이렇게 한다.");
+        System.out.println("이렇게 한다.");
+        System.out.println("이렇게 한다.");
     }
 }
